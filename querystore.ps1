6342f49566d9af364216f09e603425a766278bfa
jspilinek <101371702+jspilinek@users.noreply.github.com>
2022-03-15 13:19:57 -0500

    00_startup.ps1

--- querystore.ps1
+++ querystore.ps1
@@ -9,73 +9,8 @@ param (
 )
 
 [string]$global:script_name="PTC Query Store Report v22.02"
-[string]$global:main_page="00_sqlperf"
-[string]$global:dateFormat='yyyy-MM-dd HH:mm:ss'
-
-.\ps1\00_LoadModules.ps1
-if($LASTEXITCODE -ne 0)
-{
-    exit
-}
-
 #execute in same scope as sqlperf.ps1
-. .\ps1\00_initDebug.ps1
-
-$srv = New-Object ('Microsoft.SQLServer.Management.Smo.Server') $server
-
-#Change timeout to 1800 seconds (30 minutes). Default timeout is 600 seconds (10 minutes)
-$srv.ConnectionContext.StatementTimeout = $timeout
-
-if($login -eq $false){
-    DebugLog "Authentication Mode: Windows Authentication"
-}else{
-    if([Console]::CapsLock -eq $true)
-    {
-        Write-Host "Warning: CAPSLOCK is on" -ForegroundColor White -BackgroundColor Red
-    }
-    Write-Host "Enter password for $login : " -ForegroundColor Yellow -NoNewline
-    $SecurePassword = Read-Host -AsSecureString
-
-    # Create a "password pointer"
-    $PasswordPointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
-    
-    # Get the plain text version of the password
-    $PlainTextPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto($PasswordPointer)
-    
-    # Free the pointer
-    [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($PasswordPointer)
-    
-    DebugLog "Authentication Mode: SQL Server Authentication"
-    
-    $srv.ConnectionContext.LoginSecure = $FALSE
-    $srv.ConnectionContext.Login = $login
-    $srv.ConnectionContext.Password = $PlainTextPassword
-}
-
-try{
-    $ErrorActionPreference = "Stop";
-    $srv.ConnectionContext.Connect()
-} catch {
-    LogException $_.Exception $error[0].ScriptStackTrace "Login failed"
-    if([Console]::CapsLock -eq $true)
-    {
-        Write-Host "Password might be wrong because CAPSLOCK is on" -ForegroundColor White -BackgroundColor Red
-        DebugLog "Password might be wrong because CAPSLOCK is on" -logOnly $true
-    }
-    Exit
-}
-
-$db = $srv.Databases[$database]
-
-if(!$db){
-    DebugLog "Database '$database' not found!" -error $true
-    DebugLog "Available databases are:"
-    $srv.Databases | SELECT Name
-    Exit
-}
-
-#execute in same scope as sqlperf.ps1
-. .\ps1\00_values.ps1
+. .\ps1\00_startup.ps1
 
 DebugLog "Generating $main_page.html"
 
