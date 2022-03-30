9b3ca2fb4a653d282ed535e136601db1d00f99e4
jspilinek <101371702+jspilinek@users.noreply.github.com>
2022-03-30 09:29:04 -0500

    Check if row compression is available

--- ps1/RowCompression.ps1
+++ ps1/RowCompression.ps1
@@ -2,6 +2,31 @@ $title = "Row Compression"
 
 Header $title
 
+[bool]$RowCompressionAvailable = $false
+
+# SQL Server 2017 or later
+if ($ProductMajorVersion -ge 14) {
+    $RowCompressionAvailable = $true
+    DebugLog "RowCompressionAvailable: SQL Server 2017 or later" -logOnly $true
+}
+# Azure SQL
+elseif ($ProductName -eq "AzureSQL") {
+    $RowCompressionAvailable = $true
+    DebugLog "RowCompressionAvailable: Azure SQL" -logOnly $true
+}
+# SQL Server 2016 SP1 or later
+elseif ($BuildVersion -eq 13.0 -And $UpdateVersion -ge 4001.0) {
+    $RowCompressionAvailable = $true
+    DebugLog "RowCompressionAvailable: SQL Server 2016 SP1 or later" -logOnly $true
+}
+# Enterprise Edition
+elseif ($Edition.Split(" ")[0] -eq "Enterprise"){
+    $RowCompressionAvailable = $true
+    DebugLog "RowCompressionAvailable: Enterprise Edtion" -logOnly $true
+}
+
+if ($RowCompressionAvailable -eq $true)        
+{
 [string]$RowCompression = (Get-Content .\sql\RowCompression.sql) -join "`n"
 [string]$RowCompressionHeap = (Get-Content .\sql\RowCompressionHeap.sql) -join "`n"
 
@@ -29,5 +54,11 @@ $comments =
 "Note compressing all indexes may take a couple hours, depending on the database size."
 
 Comments $comments
+}else{
+$comments = 
+"Row compression is only available in Enterprise Edition of versions prior to SQL Server 2016 SP1"
+
+Comments $comments
+}
 
 Footer
