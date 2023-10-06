2b8bb6c14d81d2aa7529a4cc70e1e130f86987b1
jspilinek <101371702+jspilinek@users.noreply.github.com>
2023-10-06 12:59:01 -0500

    Split in memory tables to separate report

--- sql/Tables.sql
+++ sql/Tables.sql
@@ -1,46 +1,33 @@
 WITH PageSize AS (SELECT v.low/1024.0 AS value FROM master.dbo.spt_values v WHERE v.number=1 and v.type='E')
+SELECT * ,
+	CAST(ROUND(CASE WHEN A.[Rows] = 0 THEN A.[DataSpace MB] ELSE A.[DataSpace MB]/A.Rows END,3) AS NUMERIC(18,3)) AS [DataSpace/Row],
+	CAST(ROUND(CASE WHEN A.[IndexSpace MB] = 0 THEN A.[DataSpace MB] ELSE A.[DataSpace MB]/A.[IndexSpace MB] END,3) AS NUMERIC(18,3)) AS [DataSpace/IndexSpace]
+FROM (
 SELECT SCHEMA_NAME(tbl.schema_id) AS [Schema Name],
   tbl.name AS [Table Name],
-  CASE WHEN (tbl.is_memory_optimized=0) THEN p.rows
-  ELSE(SELECT ISNULL([rows],0)  as [Total Records]
-       FROM sys.hash_indexes as Ind
-       CROSS APPLY sys.dm_db_stats_properties(Ind.object_id,Ind.index_id)
-       WHERE Ind.index_id =2 AND Ind.object_id=tbl.object_id
-      )
-  END AS [Rows], 
-  CASE WHEN (tbl.is_memory_optimized=0) THEN
-        CAST(ROUND(TRY_CONVERT(float,ISNULL((SELECT PageSize.value * SUM(CASE WHEN a.type <> 1 THEN a.used_pages WHEN p.index_id < 2 THEN a.data_pages ELSE 0 END)
-        FROM sys.indexes as i
-        JOIN sys.partitions as p ON p.object_id = i.object_id and p.index_id = i.index_id
-        JOIN sys.allocation_units as a ON a.container_id = p.partition_id
-        WHERE i.object_id = tbl.object_id),0.0))/1024,3) AS NUMERIC(18,3))
-  ELSE
-        CAST(Round(TRY_CONVERT(float,isnull((SELECT (tms.[memory_used_by_table_kb])
-        FROM [sys].[dm_db_xtp_table_memory_stats] tms
-        WHERE tms.object_id = tbl.object_id), 0.0))/1024,3) AS NUMERIC(18,3))
-  END as [DataSpace MB], 
-  CASE WHEN (tbl.is_memory_optimized=0) THEN
-    CAST(ROUND(TRY_CONVERT(float,ISNULL((
+  p.rows AS [Rows], 
+  CAST(ROUND(TRY_CONVERT(float,ISNULL((SELECT PageSize.value * SUM(CASE WHEN a.type <> 1 THEN a.used_pages WHEN p.index_id < 2 THEN a.data_pages ELSE 0 END)
+    FROM sys.indexes AS i
+    JOIN sys.partitions AS p ON p.object_id = i.object_id and p.index_id = i.index_id
+    JOIN sys.allocation_units AS a ON a.container_id = p.partition_id
+    WHERE i.object_id = tbl.object_id),0.0))/1024,3) AS NUMERIC(18,3)) AS [DataSpace MB], 
+  CAST(ROUND(TRY_CONVERT(float,ISNULL((
     (SELECT SUM (used_page_count) FROM sys.dm_db_partition_stats ps WHERE ps.object_id = tbl.object_id)
     + ( CASE (SELECT count(*) FROM sys.internal_tables WHERE parent_id = tbl.object_id AND internal_type IN (202,204,207,211,212,213,214,215,216,221,222))
         WHEN 0 THEN 0
         ELSE (
-            SELECT sum(p.used_page_count)
-            FROM sys.dm_db_partition_stats p, sys.internal_tables it
-            WHERE it.parent_id = tbl.object_id AND it.internal_type IN (202,204,207,211,212,213,214,215,216,221,222) AND p.object_id = it.object_id)
+          SELECT sum(p.used_page_count)
+          FROM sys.dm_db_partition_stats p, sys.internal_tables it
+          WHERE it.parent_id = tbl.object_id AND it.internal_type IN (202,204,207,211,212,213,214,215,216,221,222) AND p.object_id = it.object_id)
         END )
     - (SELECT SUM (CASE WHEN(index_id < 2) THEN (in_row_data_page_count + lob_used_page_count + row_overflow_used_page_count) ELSE 0 END)
-        FROM sys.dm_db_partition_stats WHERE object_id = tbl.object_id)
-    ) * PageSize.value, 0.0) )/1024,3) AS NUMERIC(18,3))
-    ELSE
-      CAST(ROUND(TRY_CONVERT(float,isnull((SELECT (tms.[memory_used_by_indexes_kb])
-      FROM [sys].[dm_db_xtp_table_memory_stats] tms
-      WHERE tms.object_id = tbl.object_id), 0.0)) /1024,3) AS NUMERIC(18,3))
-  END as [IndexSpace MB],
+      FROM sys.dm_db_partition_stats WHERE object_id = tbl.object_id)
+    ) * PageSize.value, 0.0) )/1024,3) AS NUMERIC(18,3)) AS [IndexSpace MB],
   p.data_compression_desc AS [Data Compression],
-  tbl.is_memory_optimized AS [In-Memory]  
-FROM sys.tables as tbl
+  CASE WHEN p.index_id = 0 THEN 'HEAP' ELSE 'CLUSTERED' END AS [Table Type]
+FROM sys.tables AS tbl
 JOIN sys.partitions p ON p.object_id = tbl.object_id, PageSize
-WHERE (p.index_id < 2 
-  OR (tbl.is_memory_optimized=1 AND p.index_id=2))
-ORDER BY tbl.name;
+WHERE p.index_id < 2 
+  AND tbl.is_memory_optimized=0
+) A
+ORDER BY A.[Table Name];
--- sql/TablesInMemory.sql
+++ sql/TablesInMemory.sql
@@ -1,3 +1,19 @@
-SELECT tbl.name, tbl.is_memory_optimized
-FROM sys.tables as tbl
-WHERE tbl.is_memory_optimized = 1;
+SELECT SCHEMA_NAME(tbl.schema_id) AS [Schema Name],
+  tbl.name AS [Table Name],
+  (SELECT ISNULL([rows],0)  AS [Total Records]
+    FROM sys.hash_indexes AS Ind
+    CROSS APPLY sys.dm_db_stats_properties(Ind.object_id,Ind.index_id)
+    WHERE Ind.index_id =2 AND Ind.object_id=tbl.object_id
+  ) AS [Rows], 
+  CAST(Round(TRY_CONVERT(float,isnull((SELECT (tms.[memory_used_by_table_kb])
+    FROM [sys].[dm_db_xtp_table_memory_stats] tms
+    WHERE tms.object_id = tbl.object_id), 0.0))/1024,3) AS NUMERIC(18,3)) AS [DataSpace MB], 
+  CAST(ROUND(TRY_CONVERT(float,isnull((SELECT (tms.[memory_used_by_indexes_kb])
+    FROM [sys].[dm_db_xtp_table_memory_stats] tms
+    WHERE tms.object_id = tbl.object_id), 0.0)) /1024,3) AS NUMERIC(18,3)) AS [IndexSpace MB],
+  p.data_compression_desc AS [Data Compression]
+FROM sys.tables AS tbl
+JOIN sys.partitions p ON p.object_id = tbl.object_id
+WHERE tbl.is_memory_optimized=1 
+  AND p.index_id=2
+ORDER BY tbl.name;
