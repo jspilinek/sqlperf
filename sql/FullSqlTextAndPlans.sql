18bd3c9fb9bd1b2e08cfcdbd75e98398e0d83053
jspilinek <101371702+jspilinek@users.noreply.github.com>
2023-12-05 13:21:25 -0600

    Fix blank results

--- sql/FullSqlTextAndPlans.sql
+++ sql/FullSqlTextAndPlans.sql
@@ -7,7 +7,7 @@ SELECT qs.query_hash,
   SUM(qs.total_logical_reads) / SUM(qs.execution_count) AS AvgLogicalReads,
   SUM(qs.total_physical_reads) / SUM(qs.execution_count) AS AvgPhysicalReads,
   SUM(qs.total_rows) / SUM(qs.execution_count) AS AvgRows,
-  MAX(qs.last_execution_time AT TIME ZONE 'ENTER_TIME_ZONE') AS last_execution_time
+  MAX(qs.last_execution_time) AS last_execution_time
 FROM sys.dm_exec_query_stats AS qs
 WHERE qs.execution_count > 0
 group by qs.query_hash
