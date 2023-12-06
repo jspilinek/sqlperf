638d24bc262cf52b4fc4a2a0946d95bc84e8bc60
jspilinek <101371702+jspilinek@users.noreply.github.com>
2023-12-06 10:00:39 -0600

    Fix time zone format for older releases

--- ps1/getUptime.ps1
+++ ps1/getUptime.ps1
@@ -1,7 +1,11 @@
 [string]$Uptime = "unavailable"
 
 if($ProductName -ne "AzureSQL"){
-    [string]$query = (Get-Content .\sql\Uptime.sql) -join "`n"
+    if($ProductMajorVersion -ge 13){
+        [string]$query = (Get-Content .\sql\Uptime.sql) -join "`n"
+    }else{
+        [string]$query = (Get-Content .\sql\2014\Uptime.sql) -join "`n"
+    }
     . .\ps1\00_executeQuery.ps1
     $Uptime = $results.tables[0].Item('Uptime')
 }
