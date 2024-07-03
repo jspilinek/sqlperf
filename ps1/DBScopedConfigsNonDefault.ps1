03777f926275cfa642c272fedac9c497b85b7a35
jspilinek <101371702+jspilinek@users.noreply.github.com>
2024-07-03 08:56:40 -0500

    Reworked comments

--- ps1/DBScopedConfigsNonDefault.ps1
+++ ps1/DBScopedConfigsNonDefault.ps1
@@ -8,9 +8,10 @@ Header $title
 .\ps1\00_TableToHtml.ps1 -tableID 0 -link $true
 
 $comments = 
-"Non-Default new in SQL Server 2017. Can use this to check if <b>LEGACY_CARDINALITY_ESTIMATION</b> has been enabled.
-Enable with:
-<b>ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = ON;</b>"
+"Non-Default new in SQL Server 2017.
+
+Can use this to check the setting of <b>LEGACY_CARDINALITY_ESTIMATION</b>. Refer to <a href='https://www.ptc.com/en/support/article/CS299797'>CS299797</a> for details."
+
 Comments $comments
 
 Footer
